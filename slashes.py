142dbb3cac87d8a87a8e16308eb2376b245dd73f
paalmijs <108520365+paalmijs@users.noreply.github.com>
2025-02-21 19:50:21 +0200

    The first commit slashes.py

---
+++ slashes.py
@@ -0,0 +1,12 @@
+# You can write line splits in python with \n
+# You can write tab spaces in python with \t
+# You can write double quotes in python with \"
+# You can write single quotes in python with \'
+# You can write backslashes in python with \\
+
+print("Hello\nWorld")
+print("Hello\tWorld")
+print('I can\'t be mad at the World')
+print("I am a \"Python\" Programmer")
+
+# In python \ is used as an escape character.
